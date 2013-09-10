a0034cc53e7d84988090a57b226bb7dd49c401b0
jonmoore1112 <surrealvisionstudios@gmail.com>
2013-09-10 19:31:24 -0400

    adding files

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Bootstrap 101 Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="_/css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="_/css/mystyles.css" rel="stylesheet" media="screen">
+
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="../../assets/js/html5shiv.js"></script>
+      <script src="../../assets/js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="_/js/bootstrap.js"></script>
+    <script src="_/js/myscripts.js"></script>
+    
+  </body>
+</html>
